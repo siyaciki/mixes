cc92a44f1fa75276835b563c00f594c8f486dc3a
siyacikisikagaya <2900862178@qq.com>
2018-05-30 18:14:52 +0800

    ch

--- websocket/workerman/server.php
+++ websocket/workerman/server.php
@@ -1,12 +1,17 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
+header("Access-Control-Allow-Origin: *");   
+header("Access-Control-Allow-Methods: GET, POST");   
+header("Access-Control-Allow-Headers: Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With");
+
+$content=$_POST['order'];
 // 指明给谁推送，为空表示向所有在线用户推送
 $to_uid = "";
 // 推送的url地址，使用自己的服务器地址
 $push_api_url = "http://192.168.1.30:2121/";
 $post_data = array(
    "type" => "publish",
-   "content" => "play",
+   "content" => $content,
    "to" => $to_uid, 
 );
 $ch = curl_init ();
