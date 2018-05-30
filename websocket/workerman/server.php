bb3f5afd9de8a961fdd62dae535dd52768460961
siyacikisikagaya <2900862178@qq.com>
2018-05-30 11:31:45 +0800

    append
    jplayer添加，socket=>workerman框架

---
+++ websocket/workerman/server.php
@@ -0,0 +1,22 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+// 指明给谁推送，为空表示向所有在线用户推送
+$to_uid = "";
+// 推送的url地址，使用自己的服务器地址
+$push_api_url = "http://192.168.1.30:2121/";
+$post_data = array(
+   "type" => "publish",
+   "content" => "play",
+   "to" => $to_uid, 
+);
+$ch = curl_init ();
+curl_setopt ( $ch, CURLOPT_URL, $push_api_url );
+curl_setopt ( $ch, CURLOPT_POST, 1 );
+curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_data );
+curl_setopt ($ch, CURLOPT_HTTPHEADER, array("Expect:"));
+$return = curl_exec ( $ch );
+curl_close ( $ch );
+var_export($return);
+
