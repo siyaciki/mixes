ac40a83d9f47179d91b8c72e6b1ab2c813b4f8a1
siyacikisikagaya <2900862178@qq.com>
2018-06-01 13:07:17 +0800

    guestAdd追加

---
+++ guestAsks.php
@@ -0,0 +1,14 @@
+<?php
+
+include ("./dao/db.class.php");
+$db = new db();
+$sql = "select * from guest_request";
+$result = $db ->query($sql);
+
+$rows = array();
+while($row = $result->fetch_assoc()){
+    array_push($rows,$row);
+}
+$json_rows = json_encode($rows, JSON_UNESCAPED_UNICODE);
+echo $json_rows;
+?>
